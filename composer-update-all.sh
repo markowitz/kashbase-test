0d7d988b66ebc9289e342b28f94d413235c9c522
Markowitz <mac@macs-MacBook-Pro.local>
2020-08-05 03:14:40 +0100

    add composer update

---
+++ composer-update-all.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+ROOTDIR=${PWD}
+
+cd api
+echo "Running composer on the API router."
+composer update
+echo "Completed"
+
+cd ${ROOTDIR}
+cd microservices
+
+# Loop through all directories
+for d in * ; do
+    # Check if its a directory
+    if [ -d ${f} ]; then
+        COMPOSER_FILE="${d}/composer.json"
+        if [[ -f "${COMPOSER_FILE}" ]]; then
+            cd "${d}"
+            echo "Running composer on the API router."
+            composer update
+            echo "Completed"
+            cd ../..
+        fi
+    fi
+done
